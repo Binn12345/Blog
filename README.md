8b33388625ea05390147bfcda138f8d180f9cf66
Binn12345 <arvincejalbo18@gmail.com>
2025-02-02 16:13:27 +0800

    temporary push

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-dyasuhduashuidhasuihduiashidhsauhdisa
+# User Roles & Permissions
+
+## 1. Admin/Superadmin
+- **User Management**: Create, edit, delete users, assign roles  
+- **Category & Tag Management**: Manage blog categories and tags  
+- **Post Management**: Approve, edit, delete any post  
+- **Comment Moderation**: Approve, delete, or block comments  
+- **Settings Management**: Configure site settings, SEO options  
+- **Analytics & Reports**: View traffic, user activity, engagement  
+- **Backup & Security**: Manage system backups, security settings  
+
+## 2. Author/Editor
+- **Write & Edit Posts**: Create new blog posts, save drafts  
+- **Manage Own Posts**: Edit or delete only their own posts  
+- **Upload Media**: Images, videos, documents  
+- **Schedule Posts**: Publish later feature  
+- **View Comments**: Reply or moderate comments on their own posts  
+
+## 3. Registered Users (Members)
+- **Read & Comment on Posts**: Engage with blog content  
+- **User Profile Management**: Edit profile, change password  
+- **Save Favorite Posts**: Bookmark posts for later reading  
+- **Subscribe to Newsletters**: Receive updates and notifications  
+
+## 4. Guest Users (Public)
+- **View Blog Posts**: Read articles  
+- **Search & Filter**: Find posts by category, tag, or keyword  
+- **Share Posts on Social Media**: Facebook, Twitter, etc.  
--- site/index.php
+++ site/index.php
@@ -1,22 +1,22 @@
 <?php
-  session_start();
-  // require_once ('userinfo.php');
-  
+session_start();
+// require_once ('userinfo.php');
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
-<?php require_once("../components/head.php")?>
+<?php require_once("../components/head.php") ?>
 
 <body>
 
   <!-- ======= Header ======= -->
-    <?php require_once("../components/header.php")?>
+  <?php require_once("../components/header.php") ?>
   <!-- End Header -->
 
   <!-- ======= Sidebar ======= -->
-    <?php if(in_array($user['usertype'], array('0'))) require_once("../components/sidebar.php") ?>
+  <?php if (in_array($user['usertype'], array('0'))) require_once("../components/sidebar.php") ?>
   <!-- End Sidebar-->
 
   <main id="main" class="main">
@@ -35,27 +35,30 @@
       <div class="row">
 
         <!-- Left side columns -->
-      
-            <!-- <?=var_dump('<pre>',$_POST,$_SESSION,$usertype)?> -->
 
-              <?php if(in_array($user['usertype'], array('0')))  {  require_once("admin.php"); ?>
-              <?php } else if(in_array($user['usertype'], array('1')))  { require_once("user.php"); }?>
-              
+        <!-- <?= var_dump('<pre>', $_POST, $_SESSION, $usertype) ?> -->
+
+        <?php if (in_array($user['usertype'], array('0'))) {
+          require_once("admin.php"); ?>
+        <?php } else if (in_array($user['usertype'], array('1'))) {
+          require_once("user.php");
+        } ?>
 
 
-        <?php if(in_array($user['usertype'], array('0')))  require_once("adminrightside.php"); ?>
- 
- 
+
+        <?php if (in_array($user['usertype'], array('0')))  require_once("adminrightside.php"); ?>
+
+
       </div>
     </section>
 
   </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
-    <?php if(in_array($user['usertype'], array('0'))) require_once("../components/footer.php")?>
+  <?php if (in_array($user['usertype'], array('0'))) require_once("../components/footer.php") ?>
   <!-- End Footer -->
 
-  <?php require_once("../components/script.php")?>
+  <?php require_once("../components/script.php") ?>
 
 </body>
 
